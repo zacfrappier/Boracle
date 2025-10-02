e791af55b8c342ece909fc9da1d290f127e78835
zacfrappier <zacfrappier@gmail.com>
2025-10-01 23:25:14 -0700

    updated readME

--- CombinedSportsDataDaily ML/readMe.py
+++ CombinedSportsDataDaily ML/readMe.py
@@ -1,46 +1,75 @@
-# folder location  (old)
-# cd "c:\Users\zacfr\OneDrive\Desktop\docus\SCHOOL\CSUN\Arcs - Boracle\postboarding\CombinedSportsDataDaily ML"
+""" 
 
-#----------------------------  venv ---------------------------------
-#       venv - home pc (the one i built)
-#       venv2 - laptop 
+------------------------      venv     ---------------------------------
+       venv - home pc (the one i built)
+       venv2 - laptop 
 
-#----------- ------------       Pip      --------------------
-#   upgrade pip
-#       python.exe -m pip install --upgrade pip
+----------- ------------       Pip      --------------------------------
+   upgrade pip
+       python.exe -m pip install --upgrade pip
 
-#   ------------------------ Environment Start/End/-------------------------------
-#to create environment
-#  1) go to folder containing project 
-#  2) run in terminal 
-#        python -m venv venv
+   --------------------     Environment Start/End     ------------------
+to create environment
+  1) go to folder containing project 
+  2) run in terminal 
+        python -m venv venv
 
-# to start environment
-#   venv\Scripts\activate
+ to start environment
+   venv\Scripts\activate
 
-#clsoe environment 
-#   deactivate
+close environment 
+   deactivate
 
-#  ------------------------ install libraries---------------------------
-#               run this in terminal for (3) libraries
-#               pip install pandas scikit-learn altair
+----------------------    install libraries    -----------------------
+               run this in terminal for (3) libraries
+               pip install pandas scikit-learn altair
 
-# -----------------------Error Handling------------------
+-------------------    run file    -----------------------------------
+ in terminal: python *filename  
+ example: python KNN.py
+       will create html file, 'run with live server'
 
-#1)--ERROR-- incase error "cannot be loaded b/c running scripts is disabled on this system"
-#  
-# 1.1)solutionSolution- run (doesnt work for me)
-#       venv\Scripts\activate.bat
+-------------------    git add    ------------------------------------ 
 
-# 1.2)Solution -change security settings (works)
-#   Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+add all files 
+    git add .
+add single file 
+    git add <filename>
+add descriptor to upload 
+    git commit -m "super duper importants fancy pant code"
+push (final step)
+    git push
+    
+ -----------------------Error Handling--------------------------------
 
-#2) --ERROR-- ModuleNotFoundError: No module named 'tabulate'
-#   2.1)solution run
-#           pip install tabulate
+1)--ERROR-- incase error "cannot be loaded b/c running scripts is disabled on this system"
+  
+ 1.1)solutionSolution- run (doesnt work for me)
+       venv\Scripts\activate.bat
+
+ 1.2)Solution -change security settings (works)
+   Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+2) --ERROR-- ModuleNotFoundError: No module named 'tabulate'
+   
+   2.1)solution run
+           pip install tabulate
+
+3)  --ERROR-- when running 'git add .' in git bash 
+   
+    msg in terminal ---
+
+    error: 'CombinedSportsDataDaily ML/' does not have a commit checked out
+    fatal: adding files failed
+
+    3.1) Navigate into problematic folder: 
+            cd <foldername>
+         remove hidden .get folder:
+            rm -rf .git
+
+        Why was this .git file made? when adding folders to main branch, git will
+        treat them as a new submodule and auto generate this file
+       
 
 
-# --------------------- run file --------------------------------
-# in terminal: python *filename  
-# example: python KNN.py
-#       will create html file, 'run with live server'
+"""
